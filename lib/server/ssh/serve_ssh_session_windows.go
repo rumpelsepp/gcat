2b449876f05e5c4a65d11d3091dcc069a2845fa2
Stefan Tatschner <stefan@rumpelsepp.org>
2022-11-14 14:30:29 +0100

    Remove windows file

--- lib/server/ssh/serve_ssh_session_windows.go
+++
@@ -1,9 +0,0 @@
-package gssh
-
-import (
-	"github.com/gliderlabs/ssh"
-)
-
-func (c *serveSSHCommand) createPty(s ssh.Session, shell string) {
-	panic("Windows support for SSH is not implemented")
-}
