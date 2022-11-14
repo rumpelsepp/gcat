2ff38ae6535fcdd83e1ef244016ce41f1862b6e6
Stefan Tatschner <stefan@rumpelsepp.org>
2022-11-14 14:23:56 +0100

    Add all variants of unix sockets

--- lib/proxy/unix/socket.go
+++ lib/proxy/unix/socket.go
@@ -6,24 +6,24 @@ import (
 	"github.com/rumpelsepp/gcat/lib/proxy"
 )
 
-type dialer struct{}
+type unixDialer struct{}
 
-func (d *dialer) Dial(prox *proxy.Proxy) (net.Conn, error) {
+func (d *unixDialer) Dial(prox *proxy.Proxy) (net.Conn, error) {
 	return net.Dial("unix", prox.GetStringOption("Path"))
 }
 
-type listener struct {
+type unixListener struct {
 	listener net.Listener
 }
 
-func (l *listener) IsListening() bool {
+func (l *unixListener) IsListening() bool {
 	if l.listener != nil {
 		return true
 	}
 	return false
 }
 
-func (l *listener) Listen(prox *proxy.Proxy) error {
+func (l *unixListener) Listen(prox *proxy.Proxy) error {
 	ln, err := net.Listen("unix", prox.GetStringOption("Path"))
 	if err != nil {
 		return err
@@ -32,43 +32,108 @@ func (l *listener) Listen(prox *proxy.Proxy) error {
 	return nil
 }
 
-func (l *listener) Accept() (net.Conn, error) {
+func (l *unixListener) Accept() (net.Conn, error) {
 	return l.listener.Accept()
 }
 
-func (l *listener) Close() error {
+func (l *unixListener) Close() error {
 	return l.listener.Close()
+}
+
+type unixgramDialer struct{}
+
+func (d *unixgramDialer) Dial(prox *proxy.Proxy) (net.Conn, error) {
+	return net.Dial("unixgram", prox.GetStringOption("Path"))
+}
+
+type unixpacketDialer struct{}
+
+func (d *unixpacketDialer) Dial(prox *proxy.Proxy) (net.Conn, error) {
+	return net.Dial("unixpacket", prox.GetStringOption("Path"))
+}
+
+type unixpacketListener struct {
+	listener net.Listener
+}
+
+func (l *unixpacketListener) IsListening() bool {
+	if l.listener != nil {
+		return true
+	}
+	return false
+}
+
+func (l *unixpacketListener) Listen(prox *proxy.Proxy) error {
+	ln, err := net.Listen("unixpacket", prox.GetStringOption("Path"))
+	if err != nil {
+		return err
+	}
+	l.listener = ln
+	return nil
+}
+
+func (l *unixpacketListener) Accept() (net.Conn, error) {
+	return l.listener.Accept()
+}
+
+func (l *unixpacketListener) Close() error {
+	return l.listener.Close()
+}
+
+var pathOption = proxy.ProxyOption[string]{
+	Name:        "Path",
+	Description: "path to socket file",
 }
 
 func init() {
 	proxy.Registry.Add(proxy.Proxy{
 		Scheme:           "unix",
-		Description:      "connect to a unix domain socket",
+		Description:      "dial unix domain socket (SOCK_STREAM)",
 		SupportsMultiple: true,
 		Examples: []string{
 			"$ gcat unix:///tmp.sock -",
 		},
-		Dialer: &dialer{},
-		StringOptions: []proxy.ProxyOption[string]{
-			{
-				Name:        "Path",
-				Description: "path to socket file",
-			},
-		},
+		Dialer:        &unixDialer{},
+		StringOptions: []proxy.ProxyOption[string]{pathOption},
 	})
 	proxy.Registry.Add(proxy.Proxy{
 		Scheme:           "unix-listen",
-		Description:      "listen on a unix domain socket",
+		Description:      "listen unix domain socket (SOCK_STREAM)",
 		SupportsMultiple: true,
 		Examples: []string{
 			"$ gcat unix-listen:///tmp.sock -",
 		},
-		Listener: &listener{},
-		StringOptions: []proxy.ProxyOption[string]{
-			{
-				Name:        "Path",
-				Description: "path to socket file",
-			},
+		Listener:      &unixListener{},
+		StringOptions: []proxy.ProxyOption[string]{pathOption},
+	})
+	proxy.Registry.Add(proxy.Proxy{
+		Scheme:           "unixgram",
+		Description:      "dial unix domain socket (SOCK_DGRAM)",
+		SupportsMultiple: true,
+		Examples: []string{
+			"$ gcat unixgram:///tmp.sock -",
 		},
+		Dialer:        &unixgramDialer{},
+		StringOptions: []proxy.ProxyOption[string]{pathOption},
+	})
+	proxy.Registry.Add(proxy.Proxy{
+		Scheme:           "unixpacket",
+		Description:      "dial unix domain socket (SOCK_SEQPACKET)",
+		SupportsMultiple: true,
+		Examples: []string{
+			"$ gcat unixpacket:///tmp.sock -",
+		},
+		Dialer:        &unixpacketDialer{},
+		StringOptions: []proxy.ProxyOption[string]{pathOption},
+	})
+	proxy.Registry.Add(proxy.Proxy{
+		Scheme:           "unixpacket-listen",
+		Description:      "listen unix domain socket (SOCK_SEQPACKET)",
+		SupportsMultiple: true,
+		Examples: []string{
+			"$ gcat unixpacket-listen:///tmp.sock -",
+		},
+		Listener:      &unixpacketListener{},
+		StringOptions: []proxy.ProxyOption[string]{pathOption},
 	})
 }
